85eb429fa48dec6ac0ab0e0d364eee6177e1daf8
Daniele Conti <fourlastor@users.noreply.github.com>
2022-12-18 17:38:11 +0000

    Add battle music

--- core/src/main/java/io/github/fourlastor/game/level/LevelScreen.java
+++ core/src/main/java/io/github/fourlastor/game/level/LevelScreen.java
@@ -2,9 +2,13 @@ package io.github.fourlastor.game.level;
 
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import io.github.fourlastor.game.di.modules.GameModule;
 import io.github.fourlastor.game.level.input.controls.Controls;
 
 import javax.inject.Inject;
@@ -14,10 +18,13 @@ public class LevelScreen extends ScreenAdapter {
     private final Engine engine;
     private final Viewport viewport;
     private final EntitiesFactory entitiesFactory;
+    private final Music music;
 
     private final World world;
     private final Match match;
     private final boolean p2IsImpostor;
+
+    private final InputMultiplexer multiplexer;
 
     @Inject
     public LevelScreen(
@@ -25,13 +32,18 @@ public class LevelScreen extends ScreenAdapter {
             Viewport viewport,
             EntitiesFactory entitiesFactory,
             World world,
-            Match match) {
+            Match match,
+            AssetManager assetManager,
+            InputMultiplexer multiplexer) {
         this.engine = engine;
         this.viewport = viewport;
         this.entitiesFactory = entitiesFactory;
         this.world = world;
         this.match = match;
         this.p2IsImpostor = match.p1.equals(match.p2);
+        this.music = assetManager.get(GameModule.ARENA_BG);
+        music.setVolume(0.5f);
+        this.multiplexer = multiplexer;
     }
 
     @Override
@@ -52,10 +64,12 @@ public class LevelScreen extends ScreenAdapter {
         engine.addEntity(entitiesFactory.base());
         engine.addEntity(entitiesFactory.player(match.p1, Controls.Setup.P1, Player.P1));
         engine.addEntity(entitiesFactory.player(match.p2, Controls.Setup.P2, Player.P2, p2IsImpostor));
+        music.play();
     }
 
     @Override
     public void hide() {
+        music.stop();
         engine.removeAllEntities();
         engine.removeAllSystems();
     }
