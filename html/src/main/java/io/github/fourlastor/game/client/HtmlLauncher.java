f45d3c8ae64ce236a1caa16c2adb294508ab15cb
Daniele Conti <fourlastor@users.noreply.github.com>
2022-12-18 13:48:04 +0000

    Use preload logo

--- html/src/main/java/io/github/fourlastor/game/client/HtmlLauncher.java
+++ html/src/main/java/io/github/fourlastor/game/client/HtmlLauncher.java
@@ -3,6 +3,8 @@ package io.github.fourlastor.game.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badlogic.gdx.backends.gwt.preloader.Preloader;
+import com.google.gwt.core.client.GWT;
 import io.github.fourlastor.game.MyGdxGame;
 
 public class HtmlLauncher extends GwtApplication {
@@ -16,6 +18,11 @@ public class HtmlLauncher extends GwtApplication {
                 return new GwtApplicationConfiguration(true);
                 // Fixed size application:
                 //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public Preloader.PreloaderCallback getPreloaderCallback() {
+                return createPreloaderPanel(GWT.getHostPageBaseURL() + "preloadlogo.png");
         }
 
         @Override
