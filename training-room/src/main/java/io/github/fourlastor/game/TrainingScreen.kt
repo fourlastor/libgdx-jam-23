1497692fe630d0e7acfb0c51d6f86053ed10699e
Daniele Conti <fourlastor@users.noreply.github.com>
2022-12-27 16:21:30 +0100

    Implement RPC calls

--- training-room/src/main/java/io/github/fourlastor/game/TrainingScreen.kt
+++ training-room/src/main/java/io/github/fourlastor/game/TrainingScreen.kt
@@ -1,21 +1,36 @@
 package io.github.fourlastor.game
 
+import com.badlogic.ashley.core.ComponentMapper
 import com.badlogic.ashley.core.Engine
+import com.badlogic.ashley.core.Entity
 import com.badlogic.gdx.ScreenAdapter
 import com.badlogic.gdx.physics.box2d.World
+import io.github.fourlastor.game.component.BodyComponent
+import io.github.fourlastor.game.component.HpComponent
+import io.github.fourlastor.game.component.InputComponent
+import io.github.fourlastor.game.component.PlayerComponent
 import io.github.fourlastor.game.level.EntitiesFactory
 import io.github.fourlastor.game.level.Match
 import io.github.fourlastor.game.level.Player
+import io.github.fourlastor.game.level.input.controls.Command
 import io.github.fourlastor.game.level.input.controls.Controls
+import io.github.fourlastor.rpc.Action
+import io.github.fourlastor.rpc.ActionType
 import io.github.fourlastor.rpc.Actions
 import io.github.fourlastor.rpc.GameInfo
+import io.github.fourlastor.rpc.PlayerInfo
+import io.github.fourlastor.rpc.Position
 import javax.inject.Inject
 
 class TrainingScreen @Inject constructor(
     private val engine: Engine,
     private val entitiesFactory: EntitiesFactory,
     private val world: World,
-    private val match: Match,
+    match: Match,
+    private val inputs: ComponentMapper<InputComponent>,
+    private val players: ComponentMapper<PlayerComponent>,
+    private val bodies: ComponentMapper<BodyComponent>,
+    private val hps: ComponentMapper<HpComponent>,
 ) : ScreenAdapter() {
     private val p2IsImpostor: Boolean = match.p1 == match.p2
 
@@ -23,13 +38,16 @@ class TrainingScreen @Inject constructor(
         engine.update(delta)
     }
 
+    private val p1 = entitiesFactory.player(match.p1, Controls.Setup.P1, Player.P1)
+    private val p2 = entitiesFactory.player(match.p2, Controls.Setup.P2, Player.P2, p2IsImpostor)
+
     override fun show() {
         for (layer in entitiesFactory.backgroundLayers()) {
             engine.addEntity(layer)
         }
         engine.addEntity(entitiesFactory.base())
-        engine.addEntity(entitiesFactory.player(match.p1, Controls.Setup.P1, Player.P1))
-        engine.addEntity(entitiesFactory.player(match.p2, Controls.Setup.P2, Player.P2, p2IsImpostor))
+        engine.addEntity(p1)
+        engine.addEntity(p2)
     }
 
     override fun hide() {
@@ -43,10 +61,43 @@ class TrainingScreen @Inject constructor(
     }
 
     fun update(actions: Actions) {
-        TODO("Not yet implemented")
+        inputs.get(p1).command = actions.p1.asCommand()
+        inputs.get(p2).command = actions.p2.asCommand()
+        render(1f / 60f)
     }
 
     fun state(): GameInfo {
-        TODO("Not yet implemented")
+        return GameInfo.newBuilder().apply {
+            p1 = playerInfo(this@TrainingScreen.p1)
+            p2 = playerInfo(this@TrainingScreen.p2)
+        }.build()
     }
+
+    private fun playerInfo(entity: Entity): PlayerInfo? {
+        val playerComponent = players.get(entity)
+        val body = bodies.get(entity).body
+        val hp = hps.get(entity)
+        return PlayerInfo.newBuilder().apply {
+            player = if (playerComponent.player.flipped) {
+                io.github.fourlastor.rpc.Player.P2
+            } else {
+                io.github.fourlastor.rpc.Player.P1
+            }
+            position = Position.newBuilder().apply {
+                x = body.position.x
+                y = body.position.y
+            }.build()
+
+            health = hp.hp.toFloat() / hp.maxHp.toFloat()
+        }.build()
+    }
+}
+
+private fun Action.asCommand(): Command = when (this.type) {
+    null -> Command.NONE
+    ActionType.Pass -> Command.NONE
+    ActionType.Left -> Command.LEFT
+    ActionType.Right -> Command.RIGHT
+    ActionType.Attack -> Command.ATTACK
+    ActionType.UNRECOGNIZED -> Command.NONE
 }
