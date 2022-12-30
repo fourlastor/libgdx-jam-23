5741811dff3ed9a09830fb53fc247912ef938084
Michele Conti <11929535+mikcnt@users.noreply.github.com>
2022-12-30 15:02:31 +0100

    Add `RpcClient`

--- trainer/src/client.py
+++ trainer/src/client.py
@@ -1,18 +1,25 @@
 import grpc
 
-from trainer.src.generated.game_pb2 import Action, Attack, Left, Actions
+from trainer.src.generated.game_pb2 import Action, Actions, GameInfo
 from trainer.src.generated.game_pb2_grpc import GameStub
+from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 
 
-def run():
-    with grpc.insecure_channel('localhost:8980') as channel:
-        stub = GameStub(channel)
-        state = stub.Update(Actions(
-            p1 = Action(type=Attack),
-            p2 = Action(type=Left),
-        ))
-        print(state)
+class RpcClient:
+    def __init__(self, address: str, port: int):
+        self.target = f"{address}:{port}"
 
+    def update(self, action_1, action_2) -> GameInfo:
+        with grpc.insecure_channel(self.target) as channel:
+            stub = GameStub(channel)
+            return stub.Update(
+                Actions(
+                    p1=Action(type=action_1),
+                    p2=Action(type=action_2),
+                )
+            )
 
-if __name__ == "__main__":
-    run()
+    def reset(self) -> GameInfo:
+        with grpc.insecure_channel(self.target) as channel:
+            stub = GameStub(channel)
+            return stub.Reset(google_dot_protobuf_dot_empty__pb2.Empty())
